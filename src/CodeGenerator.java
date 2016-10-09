ec06996fa869956a66a955b779aaf26c42d1f4f9
lavenderstream <136625214@qq.com>
2016-10-09 16:07:37 +0800

    first commit

---
+++ src/CodeGenerator.java
@@ -0,0 +1,85 @@
+import com.intellij.psi.*;
+import com.intellij.psi.codeStyle.JavaCodeStyleManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by tiny on 10/9/2016.
+ */
+public class CodeGenerator {
+    private final PsiClass mClass;
+    private final List<PsiField> mFields;
+
+    public CodeGenerator(PsiClass psiClass, List<PsiField> fields) {
+        mClass = psiClass;
+        mFields = fields;
+    }
+
+    public void generate() {
+        PsiElementFactory elementFactory = JavaPsiFacade.getElementFactory(mClass.getProject());
+
+        List<PsiMethod> methods = new ArrayList<>();
+
+        for (PsiField field : mFields) {
+            PsiMethod getter =
+                    elementFactory.createMethodFromText(generateGetterMethod(field), mClass);
+            PsiMethod setter =
+                    elementFactory.createMethodFromText(generateSetterMethod(field), mClass);
+            methods.add(getter);
+            methods.add(setter);
+        }
+
+        JavaCodeStyleManager styleManager = JavaCodeStyleManager.getInstance(mClass.getProject());
+
+        for (PsiMethod method : methods) {
+            styleManager.shortenClassReferences(mClass.add(method));
+        }
+    }
+
+    private static String generateGetterMethod(PsiField field) {
+        StringBuilder sb = new StringBuilder("@Bindable\npublic ");
+        sb.append(field.getType().getPresentableText());
+
+        StringBuilder sb2 = new StringBuilder(field.getName());
+
+        // verify that the first character is an 'm' or an 's' and the second is uppercase
+        if ((sb2.charAt(0) == 'm' || sb.charAt(0) == 's') && sb2.charAt(1) < 97) {
+            sb2.deleteCharAt(0);
+        }
+        sb2.setCharAt(0, Character.toUpperCase(sb2.charAt(0)));
+
+        sb.append(" get").append(sb2.toString()).append("() { return ").append(field.getName())
+                .append("; }");
+        return sb.toString();
+    }
+
+    private static String generateSetterMethod(PsiField field) {
+        StringBuilder sb = new StringBuilder("public void set");
+        StringBuilder sb2 = new StringBuilder(field.getName());
+        boolean needsThis = true;
+
+        // verify that the first character is an 'm' or an 's' and the second is uppercase
+        if ((sb2.charAt(0) == 'm' || sb.charAt(0) == 's') && sb2.charAt(1) < 97) {
+            sb2.deleteCharAt(0);
+            needsThis = false;
+        }
+        sb2.setCharAt(0, Character.toUpperCase(sb2.charAt(0)));
+        String paramUpper = sb2.toString();
+
+        sb2.setCharAt(0, Character.toLowerCase(sb2.charAt(0)));
+        String param = sb2.toString();
+
+        sb.append(paramUpper).append("(").append(field.getType().getPresentableText()).append(" ")
+                .append(param).append(") { ");
+
+        if (needsThis) {
+            sb.append("this.");
+        }
+
+        sb.append(field.getName()).append(" = ").append(param).append(";");
+        sb.append("notifyPropertyChanged(BR." +field.getName()+ "); }");
+
+        return sb.toString();
+    }
+}
---
+++ src/DbSetterAction.java
@@ -0,0 +1,66 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.psi.*;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.ui.CollectionListModel;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DbSetterAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        PsiClass psiClass = getPsiClassFromEvent(e);
+
+        PsiField[] allFields = psiClass.getAllFields();
+        PsiField[] fields = new PsiField[allFields.length];
+
+        int i = 0;
+
+        for (PsiField field : allFields) {
+            if (!field.hasModifierProperty(PsiModifier.STATIC)) {
+                fields[i++] = field;
+            }
+        }
+
+        fields = Arrays.copyOfRange(fields, 0, i);
+        CollectionListModel<PsiField> myFields  = new CollectionListModel<PsiField>(fields);;
+
+        generateAccessors(psiClass, myFields.getItems());
+
+       /* dialog.show();
+
+        if (dialog.isOK()) {
+            generateAccessors(psiClass, dialog.getSelectedFields());
+        }*/
+    }
+
+
+    private void generateAccessors(final PsiClass psiClass, final List<PsiField> fields) {
+        new WriteCommandAction.Simple(psiClass.getProject(), psiClass.getContainingFile()) {
+            @Override
+            protected void run() throws Throwable {
+                new CodeGenerator(psiClass, fields).generate();
+            }
+        }.execute();
+    }
+
+    private PsiClass getPsiClassFromEvent(AnActionEvent event) {
+        PsiFile psiFile = event.getData(LangDataKeys.PSI_FILE);
+        Editor editor = event.getData(PlatformDataKeys.EDITOR);
+
+        if (psiFile == null || editor == null) {
+            return null;
+        }
+
+        int offset = editor.getCaretModel().getOffset();
+        PsiElement element = psiFile.findElementAt(offset);
+
+        return PsiTreeUtil.getParentOfType(element, PsiClass.class);
+    }
+}
